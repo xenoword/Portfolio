d1a15bf157c586f3d0b398470314d53e81676bdf
mortierq <mortierq@etu.univ-smb.fr>
2022-06-03 22:59:32 +0200

    test de diagonal mieux faite

--- src/components/Backgrounds/BodyDiagonal.vue
+++ src/components/Backgrounds/BodyDiagonal.vue
@@ -12,13 +12,23 @@ export default {
   props : {
      alignX : {
          type : String,
-         required: false,
-         default: 'right'
+         required : false,
+         default : 'right'
      },
      alignY : {
          type : String,
-         required: false,
-         default: 'top'
+         required : false,
+         default : 'top'
+     },
+     IsPrecededByBody : {
+         type : Boolean,
+         required : false,
+         default : true
+     },
+     IsSucceededByBody : {
+         type : Boolean,
+         required : false,
+         default : true
      }
   }
 }
@@ -26,14 +36,20 @@ export default {
 
 
 <template>
-    <div class="bgnd_bodydiagonal_container" :class="alignX, alignY">
+    <div class="bgnd_bodydiagonal_container" :class="[alignX, alignY, IsPrecededByBody ? 'with_line_top' : '', IsSucceededByBody ? 'whit_line_bottom' : '']">
         <div class="diagonal">
             <div class="left">
+                <div class="lone" v-if="IsPrecededByBody"></div>
+                <div class="ltwo" v-if="IsPrecededByBody"></div>
+                <div class="lthree" v-if="IsPrecededByBody"></div>
                 <Trapeze class="one" :height="'25vw'" :width="'100vw'" :color="'rgb(60,60,60)'"/>
                 <Trapeze class="two" :height="'25vw'" :width="'96vw'" :color="'rgb(82,27,170)'"/>
                 <Trapeze class="three" :height="'25vw'" :width="'90vw'" :color="'rgb(82,27,170)'"/>
             </div>
             <div class="right">
+                <div class="lone" v-if="IsSucceededByBody"></div>
+                <div class="ltwo" v-if="IsSucceededByBody"></div>
+                <div class="lthree" v-if="IsSucceededByBody"></div>
                 <Trapeze class="one" :height="'25vw'" :width="'100vw'" :color="'rgb(60,60,60)'" :IsReverse="true"/>
                 <Trapeze class="two" :height="'25vw'" :width="'96vw'" :color="'rgb(82,27,170)'" :IsReverse="true"/>
                 <Trapeze class="three" :height="'25vw'" :width="'90vw'" :color="'rgb(82,27,170)'" :IsReverse="true"/>
@@ -79,5 +95,62 @@ export default {
 .bgnd_bodydiagonal_container > .diagonal > .right{
     bottom: -20px;
     transform: rotateY(180deg)translateX(-25%);
+}
+/* with line at top */
+/* à faire : margin négatif si pas de ligne */ 
+.bgnd_bodydiagonal_container.with_line_top > .diagonal{
+    min-height: 27.4vw;
+}
+.bgnd_bodydiagonal_container.with_line_top > .diagonal > .left > :nth-child(n+4){
+    margin-top: 2.4vw;
+}
+
+/* with line at bottom */
+
+.left > :nth-child(-n+3)::after{
+    filter:drop-shadow(10px 0px 5px rgb(50,50,50));
+    content: " ";
+    position: absolute;
+    right: -20px;
+    top: -5%;
+    height: 110%;
+    width: 100%;
+}
+.left > .lone{
+    padding-left: 17%;
+    padding-top: 0;
+    width: 58vw;
+    height: 3.6vw;
+    background-color: rgb(60,60,60);
+    z-index: 1;
+}
+.left > .lone::after{
+    border-left: 10.5vw solid rgb(60,60,60);
+    border-right: 10.5vw solid rgb(60,60,60);
+    background-color: rgb(60,60,60);
+}
+.left > .ltwo{
+    padding-left: 17%;
+    width: 54vw;
+    height: 3.6vw;
+    background-color: rgb(82,27,170);
+    z-index: 1;
+}
+.left > .ltwo::after{
+    border-left: 8vw solid rgb(82,27,170);
+    border-right: 8vw solid rgb(82,27,170);
+    background-color: rgb(82,27,170);
+}
+.left > .lthree{
+    padding-left: 17%;
+    width: 48vw;
+    height: 3.6vw;
+    background-color: rgb(82,27,170);
+    z-index: 1;
+}
+.left > .lthree::after{
+    border-left: 10.5vw solid rgb(82,27,170);
+    border-right: 10.5vw solid rgb(82,27,170);
+    background-color: rgb(82,27,170);
 }
 </style>
